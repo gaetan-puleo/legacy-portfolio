9f508c5bdc95932e6437ef79c2d30382babe607c
Gaetan Puleo <fake@email.com>
2023-05-28 14:06:30 +0200

    A long time ago in a galaxy, far, far away...

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var toggleNav =  function() {
+    document.getElementById('navigation').classList.toggle('active');
+};
+
+
+smoothScroll.init({
+    selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+    selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+    speed: 300, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+    callback: function ( toggle, anchor ) {} // Function to run after scrolling
+});
